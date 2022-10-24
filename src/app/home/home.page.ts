f6e88cb80bcb040e7199ac8ec37b3a7fda22307f
Mancheño Gil, Andrés <95417346+amangil1010@users.noreply.github.com>
2022-10-24 10:03:19 +0200

    Calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,38 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  num: number;
+  num2: number;
+  numS: number;
+  numR: number;
+  numM: number;
+  numD: number;
+
+
+  suma() {
+    this.numS= this.num + this.num2;
+    console.log(this.numS);
+  }
+
+  resta() {
+    this.numR= this.num - this.num2;
+    console.log(this.numR);
+  }
+
+  multiplicacion() {
+    this.numM= this.num * this.num2;
+    console.log(this.numM);
+  }
+
+  division() {
+    this.numD= this.num / this.num2;
+    console.log(this.numD);
+  }
+
+  reinicia() {
+    this.num=null;
+    this.num2=null;
+    this.numR=null;
+  }
 
 }
